1ede0835f81f77b011337449777c62eb85303efa
mitrathakker <mitra_thakker@yahoo.com>
2018-04-22 18:58:45 +0530

    Updated travis.yml

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(name="DataStructuresInPython",
+      version="0.1",
+      description="Implementation of basic Data Structures in Python 3.6",
+      author="Mitra Thakker",
+      author_email="",
+      url="",
+      packages=find_packages())
