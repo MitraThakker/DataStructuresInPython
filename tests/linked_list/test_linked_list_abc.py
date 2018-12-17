633a4b33179e8d769f48da4957ce8ad468eb3747
MitraThakker <mitra_thakker@yahoo.com>
2018-12-17 12:56:37 +0530

    Refactored as follows
    1. Added back @abstractmethod in linked_list_abc
    2. Added LinkedList.__abstractmethods__ definition in TestLinkedList.setUp()

--- tests/linked_list/test_linked_list_abc.py
+++ tests/linked_list/test_linked_list_abc.py
@@ -6,6 +6,7 @@ from src.linked_list.linked_list_abc import LinkedList
 class TestLinkedList(unittest.TestCase):
 
     def setUp(self):
+        LinkedList.__abstractmethods__ = frozenset()
         self.linked_list = LinkedList()
 
     def tearDown(self):
